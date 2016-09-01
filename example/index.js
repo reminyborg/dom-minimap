b7f74eca817c59bdd1d031ef98c9a9c1e4cf7243
Remi Nyborg <remi@nyborg.priv.no>
2016-09-01 11:39:38 +0200

    Better implementation more options

--- example/index.js
+++ example/index.js
@@ -3,15 +3,16 @@ const lorem = require('lorem-ipsum')
 const randomInt = require('random-int')
 const minimap = require('../')
 
-var map = minimap()
+const map = minimap({ paddingBottom: '2px' })
+const width = 90
 
 var content = Array.from(Array(10).keys()).map((index) => lorem({ units: 'paragraphs', suffix: ':', count: randomInt(1, 23) }).split('::'))
 var view = yo`
   <div>
-    <main id="minimap-content" style="position:absolute;top:0;left:0;width:85%;height:100%;overflow:auto">
-      ${content.map((con, index) => yo`<dl class="section" data-section="${index}" style="background-color:lightgrey;margin:30px;margin-bottom:100px;padding:20px;"><dt>${index}</dt><dd>${con.map((c) =>yo`<p>${c}</p>`)}</dd></dl>`)}
+    <main id="minimap-content" style="position:absolute;top:0;left:0;width:calc(100% - ${width}px);height:100%;overflow:auto">
+      ${content.map((con, index) => yo`<dl class="minimap-section" data-minimap-title="${index}" style="background-color:lightgrey;margin:30px;padding:20px;"><dt>${index}</dt><dd>${con.map((c) =>yo`<p>${c}</p>`)}</dd></dl>`)}
     </main>
-    <aside style="position:absolute;top:0;left:85%;width:15%;height:100%">
+    <aside style="position:absolute;top:0;left:calc(100% - ${width}px);right:0;height:100%">
       ${map}
     </aside>
   </div>
--- index.js
+++ index.js
@@ -1,46 +1,71 @@
 const yo = require('yo-yo')
 const onload = require('on-load')
+const nanoraf = require('nanoraf')
 // const resizeEvent = require('element-resize-event')
 
 module.exports = minimap
 
 function minimap (opts) {
   opts = opts || {}
-  opts.sections = opts.sections || ((container) => Array.prototype.slice.call(container.getElementsByClassName('section')))
-  opts.title = opts.title || ((section) => section.getAttribute('data-section'))
-  var container
-
+  opts.sections = opts.sections || 'minimap-section'
+  if (typeof opts.sections !== 'function') {
+    var sectionName = opts.sections
+    opts.sections = (container) => Array.prototype.slice.call(container.getElementsByClassName(sectionName))
+  }
+  opts.title = opts.title || 'data-minimap-title'
+  if (typeof opts.title !== 'function') {
+    var titleName = opts.title 
+    opts.title = (section) => section.getAttribute(titleName)
+  }
+  opts.container = opts.container || 'minimap-content'
+  
   var element = document.createElement('div')
   element.style.flex = '1'
 
+  const render = nanoraf(renderMap)
+  var state = { element: element, opts: opts } 
+
   onload(element, () => {
-    container = document.getElementById('minimap-content')
-    render()
+    state.container = typeof opts.container === 'string' ? document.getElementById(opts.container) : opts.container
+    update({ container: state.container })
   })
-
-  function render () {
-    var content = 'loading'
-    if (container) {
-      content = sections(container).map((section) => {
-        return yo`<div style="position:absolute;background-color:lightgrey;top:${section.top}%;bottom:${section.bottom}%;left:5px;right:5px;overflow:hidden;color:grey;font-size:10px;padding-left:2px;border-radius:2px">${section.title}</div>`
-      })
-    }
-
-    yo.update(element, yo`<div style='position:relative;height:100%'>${content}</div>`)
-  }
-
-  function sections (container) {
-    var cHeight = container.scrollHeight
-    var cBounds = container.getBoundingClientRect()
-    return opts.sections(container).map((section) => {
-      var bounds = section.getBoundingClientRect()
-      return {
-        top: ((bounds.top - cBounds.top) / cHeight) * 100,
-        bottom: (1 - (bounds.bottom - cBounds.top) / cHeight) * 100,
-        title: opts.title(section)
-      }
-    })
+  
+  function update (partialState) {
+    var newState = Object.assign({}, state, partialState)
+    render(newState, state)
+    state = newState
   }
 
   return element
+}
+
+function renderMap (state) {
+  content = yo`<div style="margin-top:20px;text-align:center">loading</div>`
+  if (state.container) {
+    content = sections(state).map((section) => {
+      return yo`<div style="position:absolute;background-color:lightgrey;top:${section.top};bottom:${section.bottom};left:5px;right:5px;overflow:hidden;color:grey;font-size:11px;padding-left:2px;border-radius:2px">${section.title}</div>`
+    })
+  }
+
+  yo.update(state.element, yo`<div style='position:relative;height:100%'>${content}</div>`)
+}
+
+function sections (state) {
+  var container = state.container
+  var opts = state.opts
+  var cHeight = container.scrollHeight
+  var cBounds = container.getBoundingClientRect()
+  return opts.sections(container).map((section) => {
+    var bounds = section.getBoundingClientRect()
+    return {
+      top: (((bounds.top - cBounds.top) / cHeight) * 100) + '%',
+      bottom: applyPadding(((1 - (bounds.bottom - cBounds.top) / cHeight) * 100) + '%', opts.paddingBottom),
+      title: opts.title(section)
+    }
+  })
+}
+
+function applyPadding (value, padding) {
+    if (!padding) return value
+    return `calc(${value} + ${padding})`
 }
